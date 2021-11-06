90aded0d7337190250015bc1c16d4d69cc4eca7a
vmdeim <vmdeim@gmail.com>
2021-11-06 22:29:05 +0500

    Full Exampl

--- min.py
+++ min.py
@@ -1,7 +1,26 @@
 A = [3,4,2,1,0,8,9,-1,100,0]
+print(A)
 min = A.pop()
+max = min
+print(min)
+print(max)
+
+indexMin = len(A)
+indexMax = len(A)
+
 while A:
+    print(A)
     a = A.pop()
+    print(min)
+    print(max)
+    print(indexMin)
+    print(indexMax)
     if a < min:
         min = a
-print(min)
+        indexMin = len(A)
+    if a > max:
+        max = a
+        indexMax = len(A)
+
+
+
